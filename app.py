82b9a4d9490f2ba568318fd89062061b85e2f6a2
DarkPrinceWarrior <safaev.rus@gmail.com>
2021-12-21 18:30:38 +0300

    second commit

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_restful import Api
 
 app = Flask(__name__)
+api = Api(app)
 
 
 @app.route('/')
-def wassup_gradusov():  # put application's code here
-    return 'Hello World!'
+def start():
+    return render_template('index.html')
+
+@app.route('/receipt')
+def receipt_action():
+    return render_template('receipt.html')
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", debug=True)
